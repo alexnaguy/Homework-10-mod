50bd56fc6228869225d279551578e66fdfd4a031
AlexNaguy <gorniynaguy@mail.ru>
2023-07-19 14:55:18 +0300

    Добавил реализацию queue через Linked_list

---
+++ data_structure/queue.py
@@ -0,0 +1,28 @@
+from data_structure.linked_list import LinkedList
+
+
+class Queue:
+    def __init__(self):
+        self.__data = LinkedList()
+
+    def enqueue(self, item):
+        """ Добавление нового элемента item в очередь """
+        self.__data.add_last(item)
+
+    def dequeue(self):
+        """ удаление и возврат очередного элемента в порядке «первым вошел, первым вышел» (FIFO) """
+        return self.__data.remove_first()
+
+    def peek(self):
+        """ возврат(без удаления) очередного элемента в очереди в порядке FIFO """
+        item = self.__data.remove_first()
+        self.__data.add_first(item)
+        return item
+
+    def __len__(self):
+        """ возврат количества элементов в очереди """
+        return len(self.__data)
+
+    def is_empty(self):
+        """ ПРоверка очереди на пустоту """
+        return len(self.__data) == 0
